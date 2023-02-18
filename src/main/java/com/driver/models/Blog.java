e599f63554f1cfffe6263d012f9403138ffb0e97
Rushi <rushibhopale15@gmail.com>
2023-02-18 23:36:35 +0530

    completed

--- src/main/java/com/driver/models/Blog.java
+++ src/main/java/com/driver/models/Blog.java
@@ -22,7 +22,6 @@ public class Blog {
     private User user;
 
     @OneToMany(mappedBy = "blog", cascade=CascadeType.ALL)
-    @JoinColumn
     private List<Image> imageList;
 
     public Blog() {
